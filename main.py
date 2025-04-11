327cf26f9dfd64c336b34bd0412225e35141b16a
Baurzhan Turabekov <zzz@yyy.xxx>
2025-04-12 01:04:31 +0500

    united lesson2 ended

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from app.handlers import router
-
+from app.database.models import async_main
 
 async def main():
+    await async_main()
     bot = Bot(token='')
     dp = Dispatcher()
     dp.include_router(router)
